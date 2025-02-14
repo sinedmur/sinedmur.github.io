ca1283aab7cf0e44b62a312cde8e1b837ea0632d
sinedmur <sinedmur2009@gmail.com>
2025-02-14 19:24:45 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const musicBtn = document.querySelector('.btn_music')
+const menuBtn = document.querySelector('.btn_menu')
+
+
+musicBtn.addEventListener('click', function() {window.location.href = 'music.html'})
+menuBtn.addEventListener('click', function() {window.location.href = 'index.html'})
