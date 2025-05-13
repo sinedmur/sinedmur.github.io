9b875199ed80f964e60853692699305c9ef180c8
sinedmur <sinedmur2009@gmail.com>
2025-05-13 15:38:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -657,7 +657,6 @@ function setupSearch() {
 function showProducerSearchResults(producers) {
     const beatsGrid = document.getElementById('beatsGrid');
     beatsGrid.innerHTML = '';
-    
     producers.forEach(producer => {
         const producerCard = document.createElement('div');
         producerCard.className = 'producer-search-card';
